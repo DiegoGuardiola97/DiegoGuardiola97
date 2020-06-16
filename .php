a4a0b67d74fe010dae3f13ecbf86bf962325a671
DiegoGuardiola97 <66449263+DiegoGuardiola97@users.noreply.github.com>
2020-06-16 18:04:14 -0500

    Create .php

---
+++ .php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Operaciones</title>
+</head>
+<body>
+<form method="POST" action="operaciones.php">
+	<label>Ingrese el primer numero</label><br>
+	<input type="number" name="numero1"><br>
+	<label>Ingrese el segundo numero</label><br>
+	<input type="number" name="numero2"><br>
+
+<select name="operacion">
+	<option value="sumar">sumar</option>
+	<option value="restar">restar</option>
+	<option value="multiplicar">multiplicar</option>
+	<option value="dividir">dividir</option>
+</select>
+
+	<button type="submit">ver resultado</button>
+</form>
+
+</body>
+</html>
+
+<<?php 
+	$num1=$_POST['numero1'];
+	$num2=$_POST['numero2'];
+
+	$operaciones=$_POST['operacion'];
+	if ($operaciones=="sumar") {
+		$suma=$num1+$num2;
+		echo "el resulto de la suma es : ".$suma;
+	}
+	elseif ($operaciones=="restar") {
+		$resta=$num1-$num2;
+		echo "el resulto de la resta es : ".$resta;
+	}
+	elseif ($operaciones=="multiplicar") {
+		$mul=$num1*$num2;
+		echo "el resulto de la multiplicación es : ".$mul;
+	elseif ($operaciones=="dividir") {
+		$div=$num1/$num2;
+		echo "el resulto de la division es : ".$div;
+?>
